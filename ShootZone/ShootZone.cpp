8b70e9c08d538949f59028c03fd7d6d039917b36
Vildan <akbulatov.vildan@gmail.com>
2025-06-01 16:20:01 +0500

    Добавьте файлы проекта.

---
+++ ShootZone/ShootZone.cpp
@@ -0,0 +1,23 @@
+﻿#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "Map.h"
+
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(640,480), "ShootZone");
+    
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        
+        window.clear();
+        window.display();
+    }
+
+    return 0;
+}
