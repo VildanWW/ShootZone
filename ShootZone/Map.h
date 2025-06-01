404c9f1279318a32149e0f02ea987f96cd10a123
Vildan <akbulatov.vildan@gmail.com>
2025-06-01 20:38:07 +0500

    Сделал функцию, которая создаёт карту

--- ShootZone/Map.h
+++ ShootZone/Map.h
@@ -1,3 +1,38 @@
-#include <SFML\Graphics.hpp>
 #pragma once
+#include <SFML/Graphics.hpp>
 
+const int height = 15;
+const int width = 20;
+
+// Функция отрисовки карты
+void setMap(sf::Sprite& sprite, sf::RenderWindow& window) {
+    std::string arr[height] = {
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                    ",
+        "                   0",
+        "0                 00",
+        "00 000   000   00000",
+        "00000000000000000000",
+        "00000000000000000000",
+        "00000000000000000000"
+    };
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (arr[i][j] == '0') {
+                sprite.setTextureRect(sf::IntRect(64, 0, 32, 32));
+            } else {
+                sprite.setTextureRect(sf::IntRect(120, 0, 32, 32));
+            }
+            sprite.setPosition(j * 32, i * 32);
+            window.draw(sprite);
+        }
+    }
+}
--- ShootZone/ShootZone.cpp
+++ ShootZone/ShootZone.cpp
@@ -2,20 +2,31 @@
 #include <iostream>
 #include "Map.h"
 
+sf::Texture textureMap; // Глобальная текстура. Важно сделать текстуру глобальной, а не локальной, чтобы она жила дольше самого спрайта, который еще ушел в другую функцию(иначе краш). Либо передавать из main() по ссылке. void showMap(sf::RenderWindow& window) {
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(640,480), "ShootZone");
-    
+void showMap(sf::RenderWindow& window) {
+    sf::Sprite sprite;
+    sprite.setTexture(textureMap);
+    setMap(sprite, window);
+}
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(640, 480), "ShootZone");
+
+    if (!textureMap.loadFromFile("image/map.png")) { // Обязательно
+        std::cerr << "Ошибка: не удалось загрузить image/map.png" << std::endl;
+        return -1;
+    }
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
+            if (event.type == sf::Event::Closed)
                 window.close();
-            }
         }
-        
+
         window.clear();
+        showMap(window);
         window.display();
     }
 
